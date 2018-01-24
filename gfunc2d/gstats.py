1f83645d5b74a3123e34933be59971f1d193ecf6
Christian Sahlholdt <sahlholdt@gmail.com>
2018-01-24 16:56:16 +0100

    Stat-functions and temporary test code

--- gfunc2d/gstats.py
+++ gfunc2d/gstats.py
@@ -6,12 +6,21 @@ Created on Wed Jan 24 14:19:27 2018
 @author: christian
 """
 
-#import h5py
+import h5py
 import numpy as np
-#import scipy
+from scipy.stats import norm
+from scipy.optimize import newton
+import matplotlib.pyplot as plt
 
-
-#test_output = '/Users/christian/stellar_ages/Gaia_benchmark/test/output.h5'
+ids = ['HD2151', 'HD22879', 'HD10700', 'HD102870', 'HD124897', 'HD122563',
+       'HD85503', 'HD62509', 'HD113226', 'HD107328', 'HD103095', 'HD201092']
+test_output = '/Users/christian/stellar_ages/Gaia_benchmark/test/output.h5'
+#test_output = '/Users/christian/stellar_ages/Gaia_benchmark/benchmark_output_logg_PARSEC/output.h5'
+test_id = ids[0]
+with h5py.File(test_output) as out:
+    g = out['gfuncs/' + test_id][:]
+    ages = out['grid/tau'][:]
+    fehs = out['grid/feh'][:]
 
 
 def smooth_gfunc2d(g):
@@ -28,6 +37,56 @@ def norm_gfunc(g, method='maxone'):
 #    elif method == 'other_method':
 #        gnorm = ...
     else:
-        raise ValueError('Invalid normalization method')
+        raise ValueError('Unknown normalization method')
 
     return gnorm
+
+
+def gfunc_age(g, norm=True, norm_method='maxone'):
+    g_age = np.sum(g, axis=1)
+    if norm:
+        g_age = norm_gfunc(g_age, norm_method)
+
+    return g_age
+
+
+def gfunc_age_mode(g_age, age_grid):
+    ind = np.argmax(g_age)
+    age_mode = age_grid[ind]
+
+    return age_mode
+
+
+def conf_glim(conf_level):
+    assert conf_level > 0 and conf_level < 1
+
+    zero_func = lambda x: 2*norm.cdf(np.sqrt(-2*np.log(x))) - 1 - conf_level
+    glim = newton(zero_func, 0.6)
+
+    return glim
+
+
+def gfunc_age_conf(g_age, age_grid, conf_level=0.68):
+    glim = conf_glim(conf_level)
+
+    ages_lim = age_grid[g_age > glim]
+    age_conf = (ages_lim[0], ages_lim[-1])
+
+    return age_conf
+
+
+g = smooth_gfunc2d(g)
+g = norm_gfunc(g)
+
+g_age = gfunc_age(g)
+age = gfunc_age_mode(g_age, ages)
+conf = gfunc_age_conf(g_age, ages)
+print(age, conf)
+fig, ax = plt.subplots()
+ax.plot(ages, g_age)
+ax.axvline(x=age, ls='--')
+ax.fill_betweenx(y=[0, 1], x1=conf[0], x2=conf[1], alpha=0.2)
+plt.show()
+
+
+
