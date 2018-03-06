c0e9244f774b08cddba9b72d462840908e387fc9
Christian Sahlholdt <sahlholdt@gmail.com>
2018-03-06 16:03:43 +0100

    first implementation of age distribution function estimation

--- gfunc2d/gstats.py
+++ gfunc2d/gstats.py
@@ -101,3 +101,192 @@ def print_age_stats(output_h5, filename):
         pd_arr.to_csv(filename, sep='\t', index_label='#ID number',
                       header=['5', '16', 'Mode', '84', '95'],
                       float_format='%2.2f', na_rep='nan')
+
+
+def estimate_samd(gfunc_files, case='1D', betas=None, stars=None, dilut=None,
+                  figdir=None):
+    # Load data
+    g2d = []
+    for gfunc_file in gfunc_files:
+        with h5py.File(gfunc_file, 'r') as gfile:
+            tau_grid = gfile['grid/tau'][:]
+            feh_grid = gfile['grid/feh'][:]
+            for starid in gfile['gfuncs']:
+                if stars is None or starid in stars:
+                    gfunc = gfile['gfuncs/' + starid][:]
+                    gfunc = smooth_gfunc2d(gfunc)
+                    gfunc = norm_gfunc(gfunc)
+                    g2d.append(gfunc)
+    g2d = np.array(g2d)
+
+    # Make grid more coarse (optionally, increases performance)
+    if dilut is not None:
+        g2d = g2d[:, ::dilut, ::dilut]
+        tau_grid = tau_grid[::dilut]
+        feh_grid = feh_grid[::dilut]
+
+    # Number of tau/feh-values and number of stars
+    l = len(feh_grid)
+    m = len(tau_grid)
+    n = g2d.shape[0]
+
+    if case == '1D':
+        g = np.sum(g2d, axis=2)
+        k = m
+    elif case == '2D':
+        k = m*l
+        g = g2d.reshape(n, k)
+
+    #------------------------------------------------
+    # Set up for estimating age distribution phi(1:k)
+    #------------------------------------------------
+
+    # weights for integrals over theta
+    w = np.ones(k) / k
+
+    # constant prior, normalized
+    Phi = np.ones(k)
+    Phi /= np.dot(w, Phi)
+
+    # initial guess for phi and lambda
+    phi = Phi
+    lamda = -1 # this gives r_j = 0 for beta = 0
+
+    # initial beta and step
+    if betas is None:
+        beta, dbeta, beta_max = 0.01, 0.01, 10.00
+    else:
+        beta, dbeta, beta_max = betas
+
+    # max number of Newton-Raphson iterations per beta
+    max_iter = 10
+    min_tol = 1.e-20
+
+    # Gw = G matrix, with each column multiplied by w(j)
+    gw = np.zeros((n, k))
+    for j in range(k):
+        gw[:,j] = g[:,j] * w[j];
+
+    # array to hold Gwu = Gw matrix, each row divided by u(i)
+    gwu = np.zeros((n, k))
+
+    finished = False
+#    y_max_plot = 5
+#    if figdir is None:
+#        figdir = './'
+
+    # list to hold beta, L, E
+    Q = []
+    # list to hold phi (the age/age-metallicity distribution)
+    samd = []
+#    # set-up for plotting phi
+#    if case == '1D':
+#        fig, ax = plt.subplots()
+#        phi_plot, = ax.plot(tau_grid, phi)
+#        ax.set_xlabel('Age [Gyr]')
+#        ax.set_ylabel('Relative frequency')
+#        ax.set_title('beta = ' + str(round(beta, 4)))
+#        ax.axhline(1, c='g', ls='--')
+#        ax.set_xlim([0, tau_grid[-1]])
+#        ax.set_ylim([0, y_max_plot])
+#        ax.grid()
+#        fig.savefig(os.path.join(figdir, 'samd1d_0.pdf'))
+#    elif case == '2D':
+#        fig, ax = plt.subplots()
+#        dtau = tau_array[1] - tau_array[0]
+#        dfeh = feh_array[1] - feh_array[0]
+#        plot_lims = (tau_array[0]-dtau, tau_array[-1]+dtau,
+#                     feh_array[0]-dfeh, feh_array[-1]+dfeh)
+#        cax = ax.imshow(phi.reshape(m, l), origin='lower', extent=plot_lims,
+#                        aspect='auto', interpolation='none')
+#        cbar = plt.gcf().colorbar(cax)
+#        cbar.set_label('Relative frequency')
+#        ax.set_xlabel('Age [Gyr]')
+#        ax.set_ylabel('[Fe/H]')
+#        ax.set_title('beta = ' + str(round(beta, 4)))
+#        ax.grid()
+#        ax.set_xlim(plot_lims[:2])
+#        ax.set_ylim(plot_lims[2:])
+#        fig.savefig(os.path.join(figdir, 'samd2d_0.pdf'))
+
+    beta_i = 1
+    while not finished:
+        for iterr in range(max_iter):
+            u = np.dot(gw, phi)
+
+            for i in range(n):
+                gwu[i, :] = gw[i, :] / u[i]
+
+            # residuals
+            r = w * (1 + np.log(phi/Phi)) - beta * np.sum(gwu, 0) + lamda * w
+            R = np.dot(w, phi) - 1
+
+            # Hessian
+            H = np.diag(w / phi) + beta * np.dot(gwu.T, gwu)
+
+            # full matrix
+            M = np.zeros((k+1, k+1))
+            M[:k, :k] = H
+            M[-1, :k] = M[:k, -1] = w
+            h = np.append(-r, -R)
+
+            s = np.append(1/np.sqrt(np.diag(H)), 1.)
+            S = np.diag(s)
+            M1 = np.dot(np.dot(S, M), S)
+            h1 = np.dot(S, h)
+
+            con = np.linalg.cond(M1)
+            if con is np.inf:
+                finished = True
+                break
+
+            Delta1 = np.linalg.solve(M1, h1)
+            Delta = np.dot(S, Delta1)
+
+            Delta_phi = Delta[:k]
+            Delta_lambda = Delta[-1]
+
+            f = 1.
+            phi_test = phi + f * Delta_phi
+            while min(phi_test) < 0:
+                f *= 0.5
+                phi_test = phi + f * Delta_phi
+            phi = phi_test
+            lamda += f * Delta_lambda
+            if min(phi) < min_tol or beta >= beta_max:
+                finished = True
+                break
+
+        # re-normalise to avoid exponential growth of rounding errors
+        phi /= np.dot(w, phi)
+        if case == '1D':
+            samd.append(phi)
+        elif case == '2D':
+            samd.append(phi.reshape(m, l))
+
+        # entropy
+        E = np.sum(w * phi * np.log(phi / Phi))
+
+        # total negative log-likelihood
+        L = -np.sum(np.log(u))
+
+        # add to list Q
+        Q.append([beta, L, E])
+
+        # plot current phi
+        # need to reduce y-scale?
+#        if np.amax(phi) > y_max_plot*0.95:
+#            y_max_plot *= 2
+#
+#        ax.set_title('beta = ' + str(round(beta, 4)))
+#        if case == '1D':
+#            ax.set_ylim([0, y_max_plot])
+#            phi_plot.set_ydata(phi)
+#            fig.savefig(os.path.join(figdir, 'samd1d_' + str(beta_i) + '.pdf'))
+#        elif case == '2D':
+#            continue
+
+        beta += dbeta
+        beta_i += 1
+
+    return samd, Q, tau_grid, feh_grid
