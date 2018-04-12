9f86772099175ac88051d0ae1b6cce080fefb494
Christian Sahlholdt <sahlholdt@gmail.com>
2018-04-12 12:26:37 +0200

    Update example runscript

--- examples/HDrun.py
+++ examples/HDrun.py
@@ -1,21 +1,28 @@
 from gfunc2d.gfunc2dmain import gfunc2d_run
 
 # Define input file, output directory, and path to an isochrone grid
-inputfile = '/Users/christian/code/gfunc2D/examples/HDinput.txt'
-outputdir = '/Users/christian/code/gfunc2D/examples/HDoutput/'
-isogrid = '/Users/christian/isochrones/grids/YY_grid.h5'
-make_plots = True
+inputfile = '/Users/csahlholdt/code/gfunc2D/examples/HDinput.txt'
+outputdir = '/Users/csahlholdt/code/gfunc2D/examples/HDoutput/'
+isogrid = '/Users/csahlholdt/isochrones/grids/MIST/MIST_v0_pmax3.h5'
+
+# Choose the figures to be produced
+make_gplots = True
+make_hrplots = True
+hr_axes = ('logT', 'logg')
+
+# Output age summary in text file?
+output_ages = True
 
 # Set names of the parameters in the columns of the input file
 # NOTE: The names must macth the name of the corresponding parameter in the
 # isochrone grid; therefore, the effective temperature is called 'logT'.
-inputnames = ('name', 'FeH', 'FeH_unc', 'logT', 'logT_unc',
+inputnames = ('sid', 'FeHini', 'FeHini_unc', 'logT', 'logT_unc',
               'logg', 'logg_unc', 'plx', 'plx_unc', 'B', 'B_unc',
               'V', 'V_unc')
 
 # Set the parameters which are fitted to
 # This must be a subset of the above 'inputnames'
-fitnames = ('FeH', 'logT', 'logg')
+fitnames = ('FeHini', 'logT', 'V', 'plx')
 
 # Value of [alpha/Fe]
 alpha = 0.0
@@ -23,5 +30,8 @@ alpha = 0.0
 ########## END OF INPUT ##########
 ##################################
 
+if make_hrplots:
+    make_hrplots = hr_axes
+
 gfunc2d_run(inputfile, isogrid, outputdir, inputnames, fitnames,
-            alpha, make_plots)
+            alpha, make_gplots, make_hrplots)
